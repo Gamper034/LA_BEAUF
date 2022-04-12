d5cfeb75a9a42d245057b54f629b28febe18c3ed
Gamper034 <ad-dev@outlook.fr>
2022-04-12 19:28:32 +0200

    Update script.php

--- php/script.php
+++ php/script.php
@@ -2,9 +2,8 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
     
-    require '../vendor/PHPMailer/PHPMailer/src/Exception.php';
-    require '../vendor/PHPMailer/PHPMailer/src/PHPMailer.php';
-    require '../vendor/PHPMailer/PHPMailer/src/SMTP.php';
+    require '../vendor/autoload.php';
+
 
     $body_msg = "<p>Test</p>";
 
