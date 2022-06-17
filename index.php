fde0cc61d7cf429af6ffe7e9effdf83522bda525
Gamper034 <ad-dev@outlook.fr>
2022-06-17 12:08:34 +0200

    maj

--- index.php
+++ index.php
@@ -7,12 +7,10 @@
 		<title>Brasserie la Beauf</title>
 		<!-- Bootstrap -->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/bootstrap.css" rel="stylesheet">
-		<link href="css/bootstrap-reboot.css" rel="stylesheet">
-		<link href="css/bootstrap-reboot.min.css" rel="stylesheet">
-		<link href="css/bootstrap-grid.css" rel="stylesheet">
-		<link href="css/bootstrap-grid.min.css" rel="stylesheet">
+		<!-- <link href="css/bootstrap.css" rel="stylesheet"> -->
 		<link href="toastr/build/toastr.css" rel="stylesheet">
+		<link href="bootstrap-icons-1.8.2/bootstrap-icons.css" rel="stylesheet">
+    	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 		<!-- Perso  -->
 		<link href="css/style_perso.css" rel="stylesheet">
@@ -23,30 +21,43 @@
 	<body>
 
 		<!-- Nav & Image header -->
-		<header>		
-			<nav class="nav justify-content-center">		
-				<div class="responsive-logo"></div>
-					<ul id="list-nav">
-						<li class="item-nav"><a href="#apropos">A PROPOS</a></li>
-						<li class="nav-hidden"><a class="nav_hidden nav_responsive" href="">.</a></li>
-						<li class="item-nav"><a href="#nosbieres"> NOS BIERES</a></li>
-						<li class="nav-hidden" ><a href="" class="nav_hidden nav_responsive"> <img class="logo_nav nav_hidden logo_nav_responsive" src="images/logo.png" alt="logo de la beauf"> </a></li>
-						<li class="item-nav"><a href="#contact">CONTACT</a></li>
-						<li class="nav-hidden"><a class="nav_hidden nav_responsive" href="">.</a></li>
-						<li class="item-nav"><a href="#follow">NOUS SUIVRE</a></li>
+		<header>						
+			<nav class="navbar navbar-expand-md navbar-light ">
+				<a class="navbar-brand d-md-none" href="#"><img src="images/logo.png" alt="logo de la beauf" class="responsive-logo"></a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbarToggler7"
+					aria-controls="myNavbarToggler7" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="myNavbarToggler7">
+					<ul class="navbar-nav mx-auto d-flex align-items-center">
+						<li class="nav-item">
+							<a class="me-2" href="#">HOME</a>
+						</li>
+						<li class="nav-item">
+							<a class="me-2 nav-hidden pointer_none" href="#">.</a>
+						</li>
+						<li class="nav-item">
+							<a class="" href="#apropos">A PROPOS</a>
+						</li>
+						<a class="d-none d-md-block " href="#"><img class="logo_nav mx-4" src="images/logo.png" alt="logo de la beauf"></a>
+						<li class="nav-item">
+							<a class="me-2" href="#nosbieres">NOS BIERES</a>
+						</li>
+						<li class="nav-item">
+							<a class="me-2 nav-hidden pointer_none" href="#">.</a>
+						</li>
+						<li class="nav-item">
+							<a class="" href="#contact">CONTACT</a>
+						</li>
 					</ul>
-
-				<div class="btn-toggle-container" role="button" onclick="navbar()">
-					<img class="" src="images/menu-icon.svg" alt="icone menu nav">
 				</div>
-				
 			</nav>
 			<div class="img_header"></div>
 		</header>
 
 		<!-- Qui sommes nous -->
-		<div class="padding_intro">
-			<div class="container-fluid intro" id="apropos">
+		<section class="padding_intro" id="apropos">
+			<div class="container-fluid intro" >
 				<div class="row">
 					<div class="col">
 						<h2 class="paragraphe">Qui sommes nous ?</h2>
@@ -59,7 +70,7 @@
 					</div>
 				</div>
 			</div>
-		</div>
+		</section>
 		
 
 		<!-- Nos Bières -->
@@ -110,52 +121,59 @@
 		</div>
 
 		<!-- Contact -->
-		<div class="container-fluid pb-5 contact">
-			<h2 class="contact" id="contact">Rentrons en contact !</h2>
-			<div class="row pb-6">
-				<div class="col-md-6">
+		<div class="container-fluid p-2 contact">
+			<h2 class="contact mb-5" id="contact">Rentrons en contact !</h2>
+			<div class="d-flex justify-content-evenly inverse_contact">
+				<div class="col-md-6 margin_contact">
 					<div class="col-10 mx-auto">
-						<form method="post" id="form">
-							<div class="m-2">
-								<label for="nom" class="">Nom*</label>
-								<input name="nom" type="text" class="form-control" id="nom" placeholder="Entrez votre nom" required>
-							</div>
-							<div class="m-2">
-								<label for="email" class="">Email*</label>
-								<input name="email" type="email" class="form-control" id="email" placeholder="Entrez votre email" required>
-							</div>
-							<div class="m-2">
-								<label for="phone" class="">Téléphone</label>
-								<input type="text" name="phone" class="form-control" id="phone" placeholder="ex: 0612345678">
-							</div>
-							<div class="pt-3">
-								<label for="message" class="">Message*</label>
-								<textarea name="message" id="message" class="form-control" rows="10" placeholder="Entez votre message " required></textarea>
-								<small id="help" class="form-text text-muted">* Champs obligatoires</small>
-							</div>
-							<div class="pt-3 text-right">
-							<button name="submit" type="submit" value="submit" id="send_button"  class="btn btn-secondary" onclick="send_form()">Envoyer</button>
-							<!-- <div id="msgSubmit" class="h3 text-center hidden">Message Submitted!</div> -->
-							</div>
-
-						</form>
+                    	<form class="" method="post" action="php/send_mail.php">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="name" name="name" placeholder="" >
+                                <label for="floatingInput">Nom</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control" id="mail" name="mail" placeholder="" >
+                                <label for="floatingInput">Adresse mail</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="phone" name="phone" placeholder="" >
+                                <label for="floatingInput">Téléphone</label>
+                            </div>
+                            <div class="form-floating mb-4">
+                                <textarea class="form-control" placeholder="" id="message" name="message" style="height: 100px"></textarea>
+                                <label for="floatingTextarea2">Message</label>
+                            </div>
+                            
+                            <button type="submit" name="send_mail" class=" btn btn-dark text-white"><i class="bi bi-send"> </i>Envoyer</button>   
+                        </form>
 					</div>
 				</div>
 
-				<div class="col-md-6">
-					<div class=" col-10 mx-auto pt-5 google_responsive">
-							<!-- <iframe class="border border-secondary rounded" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.3901445677707!2d3.4567961513350642!3d43.660855179018625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b15d2cc40e4b2d%3A0x9f71d5d508ad422f!2sBrasserie%20La%20Beauf!5e0!3m2!1sfr!2sfr!4v1648658967732!5m2!1sfr!2sfr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
-							
-							<div class="d-flex align-items-left pt-3 pl-0">
-								<div class="t pl-0 col-3 p-2 localisation d-flex align-items-center justify-content-center">
-									<span class="fa-solid fa-location-dot fa-xl text-white"></span>
-								</div>
-								<div class="p-2 col-9">
-									<p><span><strong>Brasserie la beauf</strong></span> <br> 7 Rue de la Forge, <br> 34725 Saint-Félix-de-Lodez</p>
-								</div>
-							</div>						
-					</div>
-				</div>
+				<div class="">
+                    <div class=" img_contact rounded">
+                        <div class="container d-flex justify-content-center h-100">
+                            <div class="row align-self-center">
+                                <div class="text-center  mb-2">
+                                    <div class="d-flex flex-column mb-3 text-white">
+                                        <div class="maddox">la Beauf</div>
+                                        <div class="Eversthedin">Brasserie Artisanale</div>
+                                        <div class="Eversthedin mt-1">Tom Senegas - Quentin Salotti </div>
+                                        <div class="">
+                                            <span class="me-4"><i class="bi bi-geo-alt-fill me-2"></i>7 Rue de la Forge, 34725 Saint-Félix-de-Lodez</span>
+                                        </div>
+                                    </div>
+                                    
+                                
+                    
+                                    
+                                    <a href="https://www.instagram.com/brasserie_la_beauf/?hl=fr"><i class="bi bi-instagram text-white me-2 fa-lg"></i></a>
+                                    <a href="https://www.facebook.com/LaBeauf/" target="_blank"><i class="bi bi-facebook text-white me-2 fa-lg"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+				
 			</div>
 		</div>
 
@@ -165,14 +183,7 @@
 			<div class="container">
 				<div class="text-center ">
 		
-					<div class="pt-2" id="follow">
-						<span class="m-3">Suivez Nous !</span>
-						<ul class="pt-2">
-							<!-- <li><a href="" class="deco_hidden fa-brands fa-twitter text-white fa-2x " style="text-decoration: none;"></a></li> -->
-							<li><a  target="blank" href="https://www.facebook.com/LaBeauf/" class="fa-brands fa-facebook text-white fa-2x" style="text-decoration: none;"></a></li>
-							<li><a target="blank" href="https://www.instagram.com/brasserie_la_beauf/?hl=fr" class="fa-brands fa-instagram text-white fa-2x" style="text-decoration: none;"></a></li>
-						</ul>
-					</div>
+					
 						
 					</div>
 				</div>
@@ -181,62 +192,10 @@
 		
 	</body>
 
-	<script  type="text/javascript" src="js/app.js"></script>
 	<script  type="text/javascript" src="js/jquery-3.6.0.js"></script>
 	<script  type="text/javascript" src="js/form-script.js"></script>
-	<script  type="text/javascript" src="toastr/toastr.js"></script>
+	<script src="js/bootstrap.bundle.min.js"></script>
 
 </html>
 
-<script>
 
-$('#send_button').on('click', function(){
-				
-				toastr.options = {
-					"closeButton": false,
-					"debug": false,
-					"newestOnTop": false,
-					"progressBar": false,
-					"positionClass": "toast-top-right",
-					"preventDuplicates": false,
-					"onclick": null,
-					"showDuration": "300",
-					"hideDuration": "1000",
-					"timeOut": "5000",
-					"extendedTimeOut": "1000",
-					"showEasing": "swing",
-					"hideEasing": "linear",
-					"showMethod": "fadeIn",
-					"hideMethod": "fadeOut"
-				}
-
-				toastr["success"]("Message envoyé avec succès")
-		
-			})
-
-	function send_form(){
-
-		$.ajax({
-			type: 'POST',          //La méthode cible (POST ou GET)
-			url : 'php/script.php',
-			dataType: 'text', //Script Cible
-			data: {
-				nom: $('input#nom').val(),
-				email: $('input#email').val(),
-				phone: $('input#phone').val(),
-				message: $('textarea#message').val()
-			},
-			success: function(data){
-				// console.log('Envoyé');
-				// toastr.success('Demande envoyée avec succès');
-				toastr["success"]("Message envoyé avec succès");
-			}
-		});
-	}
-
-
-	
-
-
-	
-</script>
